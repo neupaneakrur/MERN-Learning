f1145869d21061881e8c9c2ea22bc4626b54150e
helloakrur <113849010+helloakrur@users.noreply.github.com>
2023-03-20 12:10:02 +0545

    event listener

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
-const heading = document.querySelector('h1'); 
-heading.innerText = " Hello Akrur"
-console.log(heading);
-const allLists = document.querySelectorAll('ul li')
-for (let i = 0; i < allLists.length; i++) {
-    const listItem = allLists[i];
-    listItem.innerText = "yes";
+// const heading = document.querySelector('h1');  //query selector for heading
+// heading.innerText = " Hello Akrur"
+// console.log(heading);
+// const allLists = document.querySelectorAll('ul li')
+// for (let i = 0; i < allLists.length; i++) {
+//     const listItem = allLists[i];
+//     listItem.innerText = "yes";
+// }
+// console.log(allLists);
+
+const increment = document.querySelector('#increment');
+let count = 0
+
+function incrementCounter (){
+    const counter1 = document.getElementById('count')
+    count++
+    counter1.innerText = count 
 }
-console.log(allLists);
+
+//attaching event listeners
+increment.addEventListener('click', incrementCounter)
