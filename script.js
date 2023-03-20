4a8195f9bd3334d317d931e3065179ee5409548d
helloakrur <113849010+helloakrur@users.noreply.github.com>
2023-03-20 12:32:51 +0545

    creating and appending element

--- script.js
+++ script.js
@@ -8,14 +8,33 @@
 // }
 // console.log(allLists);
 
-const increment = document.querySelector('#increment');
+const increment = document.querySelector('#increment')
+const decrement = document.querySelector('#decrement')
+const ulElement = document.getElementById('list-items')
+const counter1 = document.querySelector('#counter')
 let count = 0
 
 function incrementCounter (){
     const counter1 = document.getElementById('count')
     count++
     counter1.innerText = count 
+
+    //creating an element
+    const li = document.createElement('li');
+    const textNode = document.createTextNode('Senetence'+ count)
+    li.appendChild(textNode)
+
+    //append the element
+    ulElement.appendChild(li)
+}
+
+function decrementCounter (){
+    const counter1 = document.getElementById('count')
+    count--
+    counter1.innerText = count 
+
 }
 
 //attaching event listeners
 increment.addEventListener('click', incrementCounter)
+decrement.addEventListener('click', decrementCounter)
