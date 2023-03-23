8dd04cd44a2ce18935326b7e4f6ef2cc7b4d6dd8
helloakrur <113849010+helloakrur@users.noreply.github.com>
2023-03-23 06:44:54 +0545

    template literals

---
+++ templateLiterals.js
@@ -0,0 +1,12 @@
+const name = "Akrur"
+const age = 20;
+
+const message = `Name is ${name} and age is ${age} years`;
+console.log(message);
+
+
+
+const x = 20
+const y = 30
+const sum = `sum of ${x} and ${y} is ${x+y}`
+console.log(sum);
